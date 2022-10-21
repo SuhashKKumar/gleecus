5f51f22496d73d259df8cc61f3de46e540d32247
React Learner <suhashsai1999@gmail.com>
2022-10-22 00:06:24 +0530

    update table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,6 +5,12 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 import "./Table.css";
 
 const Table = () => {
+  const date = new Date()
+    .toDateString()
+    .toString()
+    .split(" ")
+    .slice(1)
+    .join(" ");
   const onGridReady = (params) => {
     // gridApi=params.api
     fetch("https://5b9f8640f5036f00142e4a2c.mockapi.io/v1/users")
@@ -12,24 +18,61 @@ const Table = () => {
       .then((res) => {
         console.log(res);
         params.api.applyTransaction({ add: res });
-      }).catch(err=>alert(err.message))
+      })
+      .catch((err) => alert(err.message));
   };
   const columns = [
-    { headerName: "Initial timely filling date", field: "" },
-    { headerName: "Primary Insurance Name", field: "" },
-    { headerName: "MRN", field: "" },
-    { headerName: "Patient", field: "name" },
-    { headerName: "BP Dates", field: "" },
-    { headerName: "Number of Comments", field: '' },
-    { headerName: "Age From Sent Date", field: "" },
-    { headerName: "Age From Visit Date", field: "" },
-    { headerName: "Episode Physician", field: "name" },
+    {
+      headerName: "Initial timely filling date",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>{date}</p>,
+    },
+    {
+      headerName: "Primary Insurance Name",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>Medicare</p>,
+    },
+    {
+      headerName: "MRN",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>A1-3126</p>,
+    },
+    { headerName: "Patient", field: "name", toolTipField: "name" },
+    {
+      headerName: "BP Dates",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>{date}</p>,
+    },
+    {
+      headerName: "Number of Comments",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>26</p>,
+    },
+    {
+      headerName: "Age From Sent Date",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>{date}</p>,
+    },
+    {
+      headerName: "Age From Visit Date",
+      field: "",
+      toolTipField: "name",
+      cellRendererFramework: (params) => <p>{date}</p>,
+    },
+    { headerName: "Episode Physician", field: "name", toolTipField: "name" },
   ];
   const specialFeatures = {
     sortable: true,
     filter: true,
     floatingFilter: true,
     flex: 1,
+    resizable: true,
   };
   return (
     <div
@@ -40,6 +83,8 @@ const Table = () => {
         onGridReady={onGridReady}
         columnDefs={columns}
         defaultColDef={specialFeatures}
+        enableBrowserTooltips={true}
+        toolTipShowDelay={{ toolTipShowDelay: 2 }}
       />
     </div>
   );
